18b22c5a828f4ad8849d5f3b6754a65636da3e1a
nhoway <n.hourmant@ovh.fr>
2017-05-11 21:03:27 +0200

    Adding dynamic social links

---
+++ application/views/blocks/social.php
@@ -0,0 +1,5 @@
+          <ul class="list-inline social-buttons">
+            <?php foreach ($items as $item) { ?>
+            <li><a href="<?php echo $item['link_target']?>"><i class="fa <?php echo $item['link_icon']?>"></i></a></li>
+            <?php } ?>
+          </ul>
--- application/views/layout.php
+++ application/views/layout.php
@@ -358,14 +358,23 @@
           <span class="copyright">Copyright &copy; Your Website 2016</span>
         </div>
         <div class="col-md-4">
-          <ul class="list-inline social-buttons">
-            <li><a href="#"><i class="fa fa-twitter"></i></a>
-            </li>
-            <li><a href="#"><i class="fa fa-facebook"></i></a>
-            </li>
-            <li><a href="#"><i class="fa fa-linkedin"></i></a>
-            </li>
-          </ul>
+          <!-- Social Buttons -->
+          <?php $this->load->view('blocks/social', array(
+            'items' => array(
+              array(
+                'link_target' => '#',
+                'link_icon'   => 'fa-twitter',
+              ),
+              array(
+                'link_target' => '#',
+                'link_icon'   => 'fa-facebook',
+              ),
+              array(
+                'link_target' => '#',
+                'link_icon'   => 'fa-linkedin',
+              ),
+            )
+          )); ?>
         </div>
         <div class="col-md-4">
           <ul class="list-inline quicklinks">
